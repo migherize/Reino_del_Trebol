9608854c78d4cfedb8de3fc792c680f92fc4bceb
migherize <miguel.herize@mykukun.com>
2023-03-02 17:46:16 -0400

    add validator id

--- models/user_model.py
+++ models/user_model.py
@@ -5,33 +5,41 @@ author: Miguel Herize
 mail: migherize@gmail.com
 """
 
-from pydantic import BaseModel, validator
 import re
+import pydantic
 
-class Grimorios(BaseModel):
-    tye_grimorios: str
 
-class MagicalAffinity(BaseModel):
-    type_affinity: str
+class Admission(pydantic.BaseModel):
+    """Solicitud de ingreso.
 
-class Admission(BaseModel):
-    """ Solicitud de ingreso.
-    
     Objeto para solicitar la admission a la academia.
-    
+
     """
+
     name: str
     surname: str
-    id: int
+    id: str
     old: int
     magical_affinity: str
 
-    @validator('name')
+    @pydantic.validator("name", "surname")
     # pylint: disable=no-self-argument
     def name_not_number_and_size_twenty(cls, v):
         """
         Nombre debe ser solo letras y maximo 20 caracteres.
         """
         if not re.match("^[^0-9]+$", v) or len(v) > 20:
-            raise ValueError('Este campo no puede contener números y tamaño maximo 20')
+            raise ValueError("El campo debe contener maximo 20 caracteres solo letras.")
+        return v
+
+    @pydantic.validator("id")
+    # pylint: disable=no-self-argument
+    def id_alphanumeric_and_size_ten(cls, v):
+        """
+        Identificador debe ser alphanumerico (letras y numeros) y maximo 10 caracteres.
+        """
+        if not re.match("^[a-zA-Z0-9]{0,10}$", v):
+            raise ValueError(
+                "El campo debe contener máximo 10 caracteres alfanuméricos."
+            )
         return v
