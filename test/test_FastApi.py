41a4d2d384d3d5ec3271c4763b4db3f2d75b807f
migherize <miguel.herize@mykukun.com>
2023-03-04 01:27:05 -0400

    add test all endpoint

--- test/test_FastApi.py
+++ test/test_FastApi.py
@@ -7,9 +7,22 @@ mail: migherize@gmail.com
 from fastapi import status
 from fastapi.testclient import TestClient
 from app import main
+from app.schemas.schemas import Admission
+from app.utils.constants import Tupla_grimorios
 
 # headers
 cabeceras_endpoint = {"accept": "application/json", "Content-Type": "application/json"}
+
+# constanst test
+
+application_test = {
+    "name": "Test",
+    "surname": "Prueba",
+    "id": "id3",
+    "old": 10,
+    "magical_affinity": "Oscuridad",
+}
+
 
 client = TestClient(app=main.app)
 
@@ -24,4 +37,89 @@ def test_home_page():
         "page": "home",
         "Version": "1.0",
         "Update Date": "March 04 2023",
+    }
+
+
+def test_application_for_admission():
+    response = client.post(
+        "/queries/send-admission",
+        headers=cabeceras_endpoint,
+        json=application_test,
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "name": response.json()["name"],
+        "magical_affinity": response.json()["magical_affinity"],
+        "grimorio": response.json()["grimorio"],
+        "status": response.json()["status"],
+    }
+
+
+def test_update_admission():
+    response = client.put(
+        "/queries/update-admission",
+        headers=cabeceras_endpoint,
+        json=application_test,
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "Academia": f" Solicitud {application_test['id']} actualiza."
+    }
+
+
+def test_update_status_admission():
+    response = client.put(
+        f"/queries/update-status-admission/{application_test['id']}",
+        headers=cabeceras_endpoint,
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "Academia": f"Solicitud {application_test['id']} estatus actualizado a Acceptado"
+    }
+
+
+def test_read_all_application():
+    response = client.get(
+        "/queries/all-request-read-all-application",
+        headers=cabeceras_endpoint,
+    )
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_read_assing_grimoire():
+    response = client.get(
+        f"/queries/read-assing-grimoire/{application_test['id']}",
+        headers=cabeceras_endpoint,
+    )
+    assert response.status_code == status.HTTP_200_OK
+    if Tupla_grimorios[0][0] in response.json():
+        assert response.json() == {
+            "Academia": f"La Solicitud {application_test['id']} tiene grimorio {Tupla_grimorios[0][0]}, {Tupla_grimorios[0][1]}."
+        }
+    elif Tupla_grimorios[1][0] in response.json():
+        assert response.json() == {
+            "Academia": f"La Solicitud {application_test['id']} tiene grimorio {Tupla_grimorios[1][0]}, {Tupla_grimorios[1][1]}."
+        }
+    elif Tupla_grimorios[2][0] in response.json():
+        assert response.json() == {
+            "Academia": f"La Solicitud {application_test['id']} tiene grimorio {Tupla_grimorios[2][0]}, {Tupla_grimorios[2][1]}."
+        }
+    elif Tupla_grimorios[3][0] in response.json():
+        assert response.json() == {
+            "Academia": f"La Solicitud {application_test['id']} tiene grimorio {Tupla_grimorios[3][0]}, {Tupla_grimorios[3][1]}."
+        }
+    elif Tupla_grimorios[4][0] in response.json():
+        assert response.json() == {
+            "Academia": f"La Solicitud {application_test['id']} tiene grimorio {Tupla_grimorios[4][0]}, {Tupla_grimorios[4][1]}."
+        }
+
+
+def test_delete_admission():
+    response = client.delete(
+        f"/queries/delete/{application_test['id']}",
+        headers=cabeceras_endpoint,
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "Academia": f"Solicitud {application_test['id']} eliminada."
     }
