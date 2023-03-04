cf3c6200728f84ef15c2f316e78b1cae484ed1bf
migherize <miguel.herize@mykukun.com>
2023-03-03 21:39:03 -0400

    change typehits and docstrings in crud

--- app/models/crud.py
+++ app/models/crud.py
@@ -11,9 +11,16 @@ from app.schemas import schemas
 import app.utils.constants as constants
 
 
-def search_and_verify(db_conn: Session, user_id: str):
+def search_and_verify(db_conn: Session, user_id: str) -> list:
     """
     funcion para buscar la solicitud de registro y validarla.
+
+    Args:
+        db_conn (Session): Parametro de sesion base de datos.
+        user_id (str): id de solicitud para buscar en base de datos.
+
+    Returns:
+        item: una lista de objeto del modelo Applicant.
     """
     item = (
         db_conn.query(models_db.Applicant)
@@ -26,9 +33,16 @@ def search_and_verify(db_conn: Session, user_id: str):
     return item
 
 
-def update_user(db_conn: Session, user: schemas.Admission):
+def update_user(db_conn: Session, user: schemas.Admission) -> list:
     """
     Actualizar la solicitud de registro.
+
+    Args:
+        db_conn (Session): Parametro de sesion base de datos.
+        user (schemas.Admission): objeto de tipo admission para actualizar el item en la base de datos.
+
+    Returns:
+        item: una lista de objeto del modelo Applicant.
     """
     item = search_and_verify(db_conn, user.id)
     item.name = user.name
@@ -42,9 +56,19 @@ def update_user(db_conn: Session, user: schemas.Admission):
     return item
 
 
-def create_user(db_conn: Session, user: schemas.Admission, grimorio: str):
+def create_user(
+    db_conn: Session, user: schemas.Admission, grimorio: str
+) -> models_db.Applicant:
     """
     Crea un estudiante en la db Academy
+
+    Args:
+        db_conn (Session): Parametro de sesion base de datos.
+        user (schemas.Admission): objeto de tipo admission para crear el item en la base de datos.
+        grimorio (str): string para grimorio del item en la base de datos.
+
+    Returns:
+        user_aplicant: un objeto del modelo Applicant.
     """
     user_aplicant = models_db.Applicant(
         name=user.name,
@@ -62,9 +86,16 @@ def create_user(db_conn: Session, user: schemas.Admission, grimorio: str):
     return user_aplicant
 
 
-def update_status(db_conn: Session, user_id: str):
+def update_status(db_conn: Session, user_id: str) -> list:
     """
     Actualizar el estatus de la solicitud de registro.
+
+    Args:
+        db_conn (Session): Parametro de sesion base de datos.
+        user_id (str): id de solicitud para buscar en la base de datos.
+
+    Returns:
+        item: una lista de objeto del modelo Applicant.
     """
     item = search_and_verify(db_conn, user_id)
     item.status = constants.STATUS_UPDATE
@@ -74,17 +105,31 @@ def update_status(db_conn: Session, user_id: str):
     return item
 
 
-def select_grimorio(db_conn: Session, user_id: str):
+def select_grimorio(db_conn: Session, user_id: str) -> list:
     """
     Consultar grimorio de una solicitud de registro.
+
+    Args:
+        db_conn (Session): Parametro de sesion base de datos.
+        user_id (str): id de solicitud para buscar en la base de datos.
+
+    Returns:
+        item: una lista de objeto del modelo Applicant.
     """
     item = search_and_verify(db_conn, user_id)
     return item
 
 
-def delete_admission(db_conn: Session, user_id: str):
+def delete_admission(db_conn: Session, user_id: str) -> list:
     """
     Eliminar una solicitud de registro.
+
+    Args:
+        db_conn (Session): Parametro de sesion base de datos.
+        user_id (str): id de solicitud para eliminar en la base de datos.
+
+    Returns:
+        item: una lista de objeto del modelo Applicant.
     """
     item = search_and_verify(db_conn, user_id)
     db_conn.delete(item)
